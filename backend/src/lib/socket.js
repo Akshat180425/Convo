7d6c45943a546dda6da282a872df1fd9fd1cfbee
Akshat Bawa <akshatbawa130806@gmail.com>
2025-06-27 22:34:24 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,6 +1,7 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import Message from "../models/message.model.js";
 
 const app = express();
 const server = http.createServer(app);
@@ -31,6 +32,36 @@ io.on("connection", (socket) => {
     console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+
+  socket.on("read_message", async ({ messageIds }) => {
+    try {
+      
+      await Message.updateMany(
+        { _id: { $in: messageIds } },
+        { $set: { status: "read" } }
+      );
+
+      const updatedMessages = await Message.find({ _id: { $in: messageIds } });
+
+      const senderMap = {};
+      updatedMessages.forEach((msg) => {
+        const sender = msg.senderId.toString();
+        if (!senderMap[sender]) senderMap[sender] = [];
+        senderMap[sender].push(msg._id);
+      });
+
+      for (const senderId in senderMap) {
+        const senderSocketId = userSocketMap[senderId];
+        if (senderSocketId) {
+          io.to(senderSocketId).emit("messages_read", {
+            messageIds: senderMap[senderId],
+          });
+        }
+      }
+    } catch (err) {
+      console.error("Error in read_message handler:", err.message);
+    }
   });
 });
 
