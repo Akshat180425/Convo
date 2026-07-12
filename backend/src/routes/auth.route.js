96cba9a8bf3129dbf3f8dacfeb1200657f8f3fe5
Akshat Bawa <akshatbawa130806@gmail.com>
2026-07-12 15:40:42 +0530

    Enhance auth routes with new endpoints and methods

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,33 @@
 import express from "express";
-import { checkAuth, login, logout, signup, updateProfile, getUserById } from "../controllers/auth.controller.js";
+import {
+  checkAuth,
+  connectGoogle,
+  forgotPassword,
+  getUserById,
+  googleLogin,
+  login,
+  logout,
+  resetPassword,
+  setPassword,
+  signup,
+  updateProfile,
+  verifyEmail,
+} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/verify-email", verifyEmail);
 router.post("/login", login);
+router.post("/google", googleLogin);
 router.post("/logout", logout);
-router.get("/user/:id", getUserById);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password", resetPassword);
+router.get("/user/:id", protectRoute, getUserById);
+router.post("/connect-google", protectRoute, connectGoogle);
+router.put("/connect-google", protectRoute, connectGoogle);
+router.put("/set-password", protectRoute, setPassword);
 router.put("/update-profile", protectRoute, updateProfile);
 
 router.get("/check", protectRoute, checkAuth);
